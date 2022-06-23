9c9c9e67debf9183faa79c6e3fc55d15c0669e85
aaron <aaron_palpallatoc@dlsu.edu.ph>
2022-06-23 23:15:52 +0800

    * added data in sample.js

--- db/sample.js
+++ db/sample.js
@@ -69,64 +69,64 @@ export function generateData(){
     ];
 
     const initialDiscussions = [
-        new Discussion({
+        {
             title: "Hi All, new to Porum here, and have a question",
             content: "Hi Guys, first of all I hope you all are ok. I have a question on how to use reddit, is it just like an Q&A platform as that seems from outside, or is info sharing also allowed and appreciated without someone asking? I am an experienced software dev, also I know a few tricks on garden agriculture, also I'm into automated systems from auto watering solutions to drones... (also that's a shame but I have an interest in international politics.) I hope I can share and also find quality information over here. Kind regards.",
             tag: {
                 name: "Introductions",
                 color: "blue",
             }
-        }),
-        new Discussion({
+        },
+        {
             title: "May 12th, 2022 COVID-19 update: 6,523 new cases, 18 new deaths, 728 hospitalized, 230 for COVID.",
             content: "6,523 confirmed and probable cases, with 5,576 positives from 70,283 tests. Of the 728 hospitalized, 230 are for COVID, 66 in the ICU, 27 Intubated, and 484 vaccinated Cases, test positivity rate, and wasterwater viral loads at winter 2020-2, and winter 2021-22 levels. Incidental COVID hospitalizations set to soon surpass Alpha/Delta levels later this week. Primary COVID hospitalizations, ICU, and intubations slightly above summer 2020 and summer 2021 levels, but below Alpha/Delta levels. No new wastewater report today, yet. Data note: With an increase in antigen at home testing, statewide probable and confirmed cases are added up and aggregated together.",
             tag: {
                 name: "Announcements",
                 color: "pink",
             }
-        }),
-        new Discussion({
+        },
+        {
             title: "Is it possible to apply for a Swiss student visa from a country outside of your home country?",
             content: "I am Japanese, currently living in France under a student visa. I will be studying in Geneva from September and need to apply for a Swiss student visa, and am wondering if I can do so from Paris, or if it is mandatory that I apply from my home country of Japan ? I sent an email to the Swiss embassy in Paris but as I expected it’s taking a while to get a response.",
             tag: {
                 name: "FAQ's",
                 color: "orange",
             }
-        }),
-        new Discussion({
+        },
+        {
             title: "Sales Person - $20/hr",
             content: "We are urgently looking for a part-time Sales Person for B2B Inside Sales. This will be a work-from-home opportunity. You would need to get on video calls or phone calls with our clients and guide them regarding the services we are offering, trying your best to crack a deal with them. We are looking for someone who is expressive and holds excellent communication skills. Also, we are looking for a US-based salesperson since you will be required to deal with our US-based clients. Pay: $20/hr For this profile, you would need a Laptop/computer, cellphone, and stable internet.",
             tag: {
                 name: "Jobs",
                 color: "brown",
             }
-        }),
-        new Discussion({
+        },
+        {
             title: "Review of the Aristocrat Restaurant",
             content: "The food is sumptuous, good servings and it’s been Aristocrat since I was a child. The Java rice was a bit dry and no flavour at all... that was a let down. Other than that, everything was a delightful gastronomical experience. As with other restaurants in the Phils, it was noisy and you have to line up for a number to get seats!",
             tag: {
                 name: "Feedback",
                 color: "purple",
             }
-        }),
+        },
     ];
 
     const initialComments = [
-        new Comment({
+        {
             content: "This is a good stop for breakfast, lunch, or dinner at this long-time Manila restaurant established in 1936. We went on a Saturday morning, and I think because it is well-known, large seating, and popular, it's difficult for service staff to keep up. Be prepared for a little arm-waving, but I think it's still well worth your while to come to this comfortable spot. For me, it was nice to see all the lola and lolo out to enjoy foods with their family. I like to come here again next time I am in Manila.",
-        }),
-        new Comment({
+        },
+        {
             content: "Food is great but service is very slow, been eating here for decades. the chicken barbecue is the best so is the halo-halo but it takes a long time for them to serve your order or bus your table. they're very busy but the inefficient staff is not helping at all.",
-        }),
-        new Comment({
+        },
+        {
             content: "Food is delicious and the prices are reasonable. Many varieties of food you can find there, and the best thing is it is open for 24 hours.",
-        }),
-        new Comment({
+        },
+        {
             content: "Good food if you are on a budget but want to try some authentic Filipino food. They also deliver, which is nice.",
-        }),
-        new Comment({
+        },
+        {
             content: "I'm always craving for Aristocrat boneless chicken barbeque and everytime I get the chance to dine at Aristocrat, it never fails to please my palate. And for a change, I tried their Kare Kare, it was cooked like what we have at home on special occasions",
-        }),
+        },
     ];
 
     var sampleUsers = [];
@@ -135,8 +135,12 @@ export function generateData(){
         sampleUsers.push(user.save());
     }
     
-
+    var sampleDiscussions = [];
+    var counter = 0;
     for(var user of sampleUsers){
-
+        initialDiscussions[counter].userId = user._id;
+        sampleDiscussions.push(new Discussion(initialDiscussions[counter++]).save());
     }
+
+    
 }
